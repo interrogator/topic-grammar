eef41b37dccb725ec6f6179f61f480f13bbefe18
daniel <mcdonaldd@unimelb.edu.au>
2016-02-11 12:57:32 +0100

    fixed root problem

--- weighter.py
+++ weighter.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 
-def make_weighted_corpus(testmode = True):
+def make_weighted_corpus(testmode = False):
     """
     make newest weighted corpus
-
     workflow:
     1. iterate over dev_test/train
     2. get function/lemma/token info
@@ -12,19 +11,15 @@ def make_weighted_corpus(testmode = True):
         b) success of previous model
     4. duplicate tokens according to weights
     5. output to file, save weights
-
     requirements:
-
     'parsed-data/functions-art.dev_test'
     'parsed-data/functions-art.train'
     'parsed-data/lemmata-art.dev_test'
     'parsed-data/lemmata-art.train'
     'parsed-data/tokens-art.dev_test'
     'parsed-data/tokens-art.train'
-
     first iteration must have no risk.weights
     non-first iterations must have current.score and best.score
-
     """
 
     import os, glob, codecs, fileinput, pickle
@@ -34,20 +29,20 @@ def make_weighted_corpus(testmode = True):
 
     # paths to our data
 
-    dvtst_functions = 'parsed-data/functions-art.dev_test'
+    #dvtst_functions = 'parsed-data/functions-art.dev_test'
     train_functions = 'parsed-data/functions-art.train'
-    dvtst_lemmata = 'parsed-data/lemmata-art.dev_test'
+    #dvtst_lemmata = 'parsed-data/lemmata-art.dev_test'
     train_lemmata = 'parsed-data/lemmata-art.train'
-    dvtst_tokens = 'parsed-data/tokens-art.dev_test'
+    #dvtst_tokens = 'parsed-data/tokens-art.dev_test'
     train_tokens = 'parsed-data/tokens-art.train'
     
     try:
-        os.remove('risk.dev_test')
+    #    os.remove('risk.dev_test')
         os.remove('risk.train')
     except OSError:
         pass
 
-    dev_test = [dvtst_functions, dvtst_lemmata, dvtst_tokens]
+    #dev_test = [dvtst_functions, dvtst_lemmata, dvtst_tokens]
     train = [train_functions, train_lemmata, train_tokens]
 
     # find out if first run or not
@@ -93,7 +88,7 @@ def make_weighted_corpus(testmode = True):
         from collections import OrderedDict
         data = pickle.load(codecs.open('risk.weights', 'rb'))
         return data[-1]
-
+        
     def determine_new_weighting(old_weights, latest_model_best):
         """
         output the new set of weights, based on the old and the prev 
@@ -101,10 +96,23 @@ def make_weighted_corpus(testmode = True):
         """
         from collections import OrderedDict
         new_weights = OrderedDict(old_weights)
-        #if first_run:
-        #    new_weights = old_weights
+        # first run is defaults
+        if first_run:
+            return old_weights
+        # if everything but root is unmodified
+        if all([v == -1 for k, v in old_weights.items()[2:]]) and \
+            old_weights['nsubj'] == 1:
+            if latest_model_best:
+                # either increment root
+                new_weights['root'] = new_weights['root'] + 1
+                return new_weights
+            else:
+                # or deincrement root and make nsubj == 2
+                new_weights['root'] = new_weights['root'] - 1
+                new_weights['nsubj'] = new_weights['nsubj'] + 1
+                return new_weights
         #else:
-        last_modified = [k for k, v in old_weights.items() if v > 0][-1]
+        last_modified = [k for k, v in old_weights.items() if v != -1][-1]
         if latest_model_best:
             new_weights[last_modified] = new_weights[last_modified] + 1
         else:
@@ -119,7 +127,7 @@ def make_weighted_corpus(testmode = True):
 
     # make some output names
     output_train_name = 'risk.train'
-    output_dt_name = 'risk.dev_test'
+    #output_dt_name = 'risk.dev_test'
     output_weight_file = 'risk.weights'
     # get old weights
     old_weights = get_weights()
@@ -130,7 +138,7 @@ def make_weighted_corpus(testmode = True):
 
     if not new_weights:
         print('Apparently finished. All functions have been weighted.')
-        return 0
+        print( 0 )
 
     #print(output_train_name)
     #print(output_dt_name)
@@ -142,30 +150,6 @@ def make_weighted_corpus(testmode = True):
     # code below repeats itself for dev_test and train
     # the idea is to match up the lemmata, tokens and functions and print the lemma form
     # by the current weight
-
-    for index, (fline, lline, tline) in enumerate(izip(open(dev_test[0], 'r'), open(dev_test[1], 'r'), open(dev_test[2], 'r'))):
-        if index > 99:
-            if testmode:
-                break
-        # print('Doing dev test, iter %d, article %d' % (num_prev, index + 1))
-        #print(fline[:25], lline[:25], tline[:25])
-        output_text = ''
-        zipped = zip(fline.split(), lline.split(), tline.split())
-        for f, l, t in zipped:
-            score = new_weights.get(f, False)
-            if score is False:
-                print('NOT FOUND:', f, l, t)
-                score = 0
-            # what should we do with pronouns? right now, print their actual written form
-            if l == '-PRON-':
-                l = t
-            if score > 0:
-                output_text += (l + ' ') * score
-
-        if not os.path.isfile(output_dt_name):
-            open(output_dt_name, 'w').close()
-        with codecs.open(output_dt_name, 'a', encoding = 'utf-8') as fo:
-            fo.write(output_text.decode('utf-8') + '\n')
 
     for index, (fline, lline, tline) in enumerate(izip(open(train[0], 'r'), open(train[1], 'r'), open(train[2], 'r'))):
         if index > 99:
@@ -194,7 +178,7 @@ def make_weighted_corpus(testmode = True):
 
     with codecs.open(output_weight_file, 'wb', encoding = 'utf-8') as fo:
         pickle.dump(weight_data, fo)
-    return 1
+    print( 1 )
 
 if __name__ == '__main__':
     make_weighted_corpus()
